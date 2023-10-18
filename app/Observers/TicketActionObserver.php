4a4b5cc8a924c2d274734965c0422c31c932666f
Luong Tuan Linh <luong.tuan.linh@sun-asterisk.com>
2023-10-19 01:31:25 +0700

    First setup

--- app/Observers/TicketActionObserver.php
+++ app/Observers/TicketActionObserver.php
@@ -11,22 +11,26 @@ class TicketActionObserver
 {
     public function created(Ticket $model)
     {
-        $data  = ['action' => 'New ticket has been created!', 'model_name' => 'Ticket', 'ticket' => $model];
-        $users = \App\User::whereHas('roles', function ($q) {
-            return $q->where('title', 'Admin');
-        })->get();
-        Notification::send($users, new DataChangeEmailNotification($data));
+        // $data  = ['action' => 'New ticket has been created!', 'model_name' => 'Ticket', 'ticket' => $model];
+        // $users = \App\User::whereHas('roles', function ($q) {
+        //     return $q->where('title', 'Admin');
+        // })->get();
+        // Notification::send($users, new DataChangeEmailNotification($data));
+
+        // TODO: Write notification logic here
     }
 
     public function updated(Ticket $model)
     {
-        if($model->isDirty('assigned_to_user_id'))
-        {
-            $user = $model->assigned_to_user;
-            if($user)
-            {
-                Notification::send($user, new AssignedTicketNotification($model));
-            }
-        }
+        // if($model->isDirty('assigned_to_user_id'))
+        // {
+        //     $user = $model->assigned_to_user;
+        //     if($user)
+        //     {
+        //         Notification::send($user, new AssignedTicketNotification($model));
+        //     }
+        // }
+
+        // TODO: Write notification logic here
     }
 }
